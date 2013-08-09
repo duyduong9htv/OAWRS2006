78d19e7d1af810c59c144c26dfac32d913d1ac13
duyduong9htv <duyduong9htv@yahoo.com>
2013-08-09 15:56:10 -0400

    Update calBearings.m

--- luars/Database/Matlab Code/calBearings.m
+++ luars/Database/Matlab Code/calBearings.m
@@ -1,10 +1,14 @@
-function bearings = calBearings(whaleLocsGuess, rcv)
-%function bearings = calBearings(whaleLocsGuess, rcv) 
-%gives the bearings (theoretical) when the trajectory of the whale is given
-%by a Nx2 vector whaleLocsGuess. 
+function bearings = calBearings(targetLocs, rcv)
+%function bearings = calBearings(targetLocs, rcv) 
+%gives the bearings (theoretical) when the trajectory of the targets is given
+%by a Nx2 vector targetLocs. 
+% INPUTS: targetLocs (Nx2) x and y coordinates of the trajectory.
+%         rcv: Nx2: x and y coordinates of the receiver. 
+% OUTPUTS: bearings: Nx1, calculated bearings. 
+% Last updated by DD Tran, Aug 9, 2013. 
 
-xx = whaleLocsGuess(:, 1); 
-yy = whaleLocsGuess(:, 2); 
+xx = targetLocs(:, 1); 
+yy = targetLocs(:, 2); 
 
 epsilon = (1e-12); 
 iota = (1 - (sign(xx - rcv(:, 1)).^2))*epsilon; %to avoid division by zero 
